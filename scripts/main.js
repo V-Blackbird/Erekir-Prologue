656b357a8df4ec940326c4cc4f15ccdb8eff2d39
V-Blackbird <113912794+V-Blackbird@users.noreply.github.com>
2022-09-19 15:52:56 +0200

    Macaroni Launcher

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+
+
+require("campaign/techTree");
+
+print("Load Complete!");
+
+
+
+
+
+
+/*
+credit(){
+  younggam: "answering tons of question",
+  deltaNedas: "refer to routorio, rtfm",
+  sk7725: "answering tons of question, refer to testers"
+}
+*/
